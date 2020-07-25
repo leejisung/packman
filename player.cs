ef681129e0e90f1d6005cbc9756e1ff30fa8f4ae
leejisung <starseed13@hanmail.net>
2020-07-25 13:56:15 +0900

    벽 구현

--- player.cs
+++ player.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
+
+    private CircleCollider2D circle2d;
+    public LayerMask layerMask;
+
     public float speed;
     private Vector3 vector;
     private bool canmove = true;
@@ -14,7 +18,7 @@ public class player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        circle2d = GetComponent<CircleCollider2D>();
     }
 
     IEnumerator movec()
@@ -28,6 +32,24 @@ public class player : MonoBehaviour
             }
 
             vector.Set(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+
+
+            RaycastHit2D hit;
+
+            Vector2 start = transform.position;
+            Vector2 end = start + new Vector2(vector.x * speed * walkcount, vector.y * speed * walkcount);   //캐릭터가 이동하고자 하는 곳
+
+            circle2d.enabled = false;
+            hit = Physics2D.Linecast(start, end, layerMask);
+            circle2d.enabled = true;
+            Debug.Log(hit.transform);
+            if (hit.transform != null)
+            {
+            
+                Debug.Log("rrr");
+                break;
+            }
+
             while (walkcount > currentwalkcount)
             {
 
@@ -47,6 +69,9 @@ public class player : MonoBehaviour
     {
         if(canmove)
         {
+
+
+
             if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
             {
                 canmove = false;
